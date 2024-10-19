d713fa99a3018f3cdaf7812204fd3c8fa6824927
Asad Babayev <esedbabayev12@gmail.com>
2024-10-19 23:09:45 +0400

    add BrowserRouter & Provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// BrowserRouter
+import { BrowserRouter } from "react-router-dom";
+
+// Provider
+import { Provider } from "react-redux";
+
+// Store
+import store from "./store/store.js"
+
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
