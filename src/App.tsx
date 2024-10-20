75d9d01d3871f2ba39a3cbc6d705c37d8199f71b
Asad Babayev <esedbabayev12@gmail.com>
2024-10-20 23:54:13 +0400

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -11,23 +11,29 @@ import Dashboard from "./pages/admin/Dashboard";
 import Products from "./pages/admin/Products";
 import Orders from "./pages/admin/Orders";
 import Features from "./pages/admin/Features";
+import UserLayout from "./components/user/layout";
+import NotFound from "./pages/not-found/NotFound";
 
 function App() {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
-      <h1 className="text-red-600">header</h1>
-
       <Routes>
+        {/* auth routes */}
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="sign-in" element={<Login />} />
           <Route path="sign-up" element={<SignIn />} />
         </Route>
+        {/* admin routes */}
         <Route path="/admin" element={<AdminLayout />}>
           <Route path="dashboard" element={<Dashboard />} />
           <Route path="products" element={<Products />} />
           <Route path="orders" element={<Orders />} />
           <Route path="features" element={<Features />} />
         </Route>
+        {/* shopping & user routes */}
+        <Route path="/shop" element={<UserLayout />}></Route>
+        {/* not found route */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
