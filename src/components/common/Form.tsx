7baa383d051d8f0d7e9900447a51e18c443e3d47
Asad Babayev <esedbabayev12@gmail.com>
2024-10-25 15:32:35 +0400

    update form component

--- src/components/common/Form.tsx
+++ src/components/common/Form.tsx
@@ -1,10 +1,131 @@
+// Types
+import { IControls } from "../../types/formControls.ts";
+import { Button } from "../ui/button.tsx";
 
-const Form = () => {
-  return (
-    <form>
-      
-    </form>
-  )
+// Components
+import { Input } from "../ui/input.tsx";
+import { Label } from "../ui/label.tsx";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select.tsx";
+import { Textarea } from "../ui/textarea.tsx";
+
+interface IProps {
+  IControls: Object;
+
+  buttonText: string;
 }
 
-export default Form
+const Form: React.FC<IProps> = ({
+  formControls,
+  formData,
+  setFormData,
+  onSubmit,
+  buttonText,
+}) => {
+  const renderInputsByComponentType = (controlItem) => {
+    let element = null;
+    const value = formData[controlItem.name] || "";
+
+    switch (controlItem.componentType) {
+      case "input":
+        element = (
+          <Input
+            id={controlItem.name}
+            name={controlItem.name}
+            placeholder={controlItem.placeholder}
+            type={controlItem.type}
+            value={value}
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                [controlItem.name]: event.target.value,
+              })
+            }
+          />
+        );
+        break;
+      case "select":
+        element = (
+          <Select
+            onValueChange={(value) =>
+              setFormData({
+                ...formData,
+                [controlItem.name]: value,
+              })
+            }
+            value={value}
+          >
+            <SelectTrigger className="w-full">
+              <SelectValue placeholder={controlItem.placeholder} />
+            </SelectTrigger>
+            <SelectContent>
+              {controlItem.options && controlItem.options.length > 0
+                ? controlItem.options.map((optionItem) => (
+                    <SelectItem key={optionItem.id} value={optionItem.id}>
+                      {optionItem.label}
+                    </SelectItem>
+                  ))
+                : null}
+            </SelectContent>
+          </Select>
+        );
+        break;
+      case "textarea":
+        element = (
+          <Textarea
+            id={controlItem.name}
+            name={controlItem.name}
+            placeholder={controlItem.placeholder}
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                [controlItem.name]: event.target.value,
+              })
+            }
+          />
+        );
+        break;
+
+      default:
+        element = (
+          <Input
+            id={controlItem.name}
+            name={controlItem.name}
+            placeholder={controlItem.placeholder}
+            type={controlItem.type}
+            value={value}
+            onChange={(event) =>
+              setFormData({
+                ...formData,
+                [controlItem.name]: event.target.value,
+              })
+            }
+          />
+        );
+        break;
+    }
+    return element;
+  };
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="flex flex-col gap-3">
+        {formControls.map((controlItem) => (
+          <div className="grid w-full gap-1.5" key={controlItem.name}>
+            <Label className="mb-1">{controlItem.label}</Label>
+            {renderInputsByComponentType(controlItem)}
+          </div>
+        ))}
+      </div>
+      <Button type="submit" className="mt-2 w-full">
+        {buttonText || "Submit"}
+      </Button>
+    </form>
+  );
+};
+
+export default Form;
