33d45abe18d05d7408f6c7b759664ea181f8ceb3
Asad Babayev <esedbabayev12@gmail.com>
2024-10-25 15:32:17 +0400

    update sign-up page

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,5 +1,44 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Form from "../../components/common/Form";
+import { signUpFormControls } from "../../config/index.js";
+
 const SignUp = () => {
-  return <div>sign-up</div>;
+  const initialState = {
+    userName: "",
+    email: "",
+    password: "",
+  };
+
+  const [formData, setFormData] = useState(initialState);
+
+  const onSubmit = () => {};
+
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Sign Up
+        </h1>
+        <p className="mt-2">
+          Already have an account?
+          <Link
+            className="font-medium text-primary hover:underline ml-2"
+            to="/auth/sign-in"
+          >
+            Sign In
+          </Link>
+        </p>
+      </div>
+      <Form
+        formControls={signUpFormControls}
+        buttonText="Create Acoount"
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
 };
 
 export default SignUp;
