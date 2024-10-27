a236a61d4bfb6c11edc42c7a72df5eb89188c320
Asad Babayev <esedbabayev12@gmail.com>
2024-10-27 23:55:50 +0400

    create sign in page

--- src/pages/auth/SignIn.tsx
+++ src/pages/auth/SignIn.tsx
@@ -1,10 +1,43 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import Form from "../../components/common/Form";
+import { loginFormControls } from "../../config/index.js";
 
 const Login = () => {
-  return (
-    <div>
-      sign-in
-    </div>
-  )
-}
+  const initialState = {
+    email: "",
+    password: "",
+  };
 
-export default Login
+  const [formData, setFormData] = useState(initialState);
+
+  const onSubmit = () => {};
+
+  return (
+    <div className="mx-auto w-full max-w-md space-y-6">
+      <div className="text-center">
+        <h1 className="text-3xl font-bold tracking-tight text-foreground">
+          Sign in into your account
+        </h1>
+        <p className="mt-2">
+          Don't have an account?
+          <Link
+            className="font-medium text-primary hover:underline ml-2"
+            to="/auth/sign-up"
+          >
+            Sign Up
+          </Link>
+        </p>
+      </div>
+      <Form
+        formControls={loginFormControls}
+        buttonText="Sign In"
+        formData={formData}
+        setFormData={setFormData}
+        onSubmit={onSubmit}
+      />
+    </div>
+  );
+};
+
+export default Login;
