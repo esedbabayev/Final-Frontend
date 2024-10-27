db973f49379082432e54329e65f1b666986c89ca
Asad Babayev <esedbabayev12@gmail.com>
2024-10-27 23:55:36 +0400

    change text

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -18,7 +18,7 @@ const SignUp = () => {
     <div className="mx-auto w-full max-w-md space-y-6">
       <div className="text-center">
         <h1 className="text-3xl font-bold tracking-tight text-foreground">
-          Sign Up
+          Create a new account
         </h1>
         <p className="mt-2">
           Already have an account?
