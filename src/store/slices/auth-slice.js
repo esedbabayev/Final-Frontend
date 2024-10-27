49b01dcf118cef23eb9abf1b53e8dfb603d94887
Asad Babayev <esedbabayev12@gmail.com>
2024-10-28 01:46:31 +0400

    create async thunk

--- src/store/slices/auth-slice.js
+++ src/store/slices/auth-slice.js
@@ -1,4 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const signUpUser = createAsyncThunk(
+  "/auth/sign-up",
+
+  async (formData) => {
+    const response = await axios.post(
+      "http://localhost:5000/api/auth/sign-up",
+      formData,
+      { withCredentials: true }
+    );
+
+    return response.data;
+  }
+);
 
 export const authSlice = createSlice({
   name: "auth",
