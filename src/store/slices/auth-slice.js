eafb94941777e5e485e84fd230333af18a3fc916
Asad Babayev <esedbabayev12@gmail.com>
2024-10-29 02:23:07 +0400

    check auth

--- src/store/slices/auth-slice.js
+++ src/store/slices/auth-slice.js
@@ -29,6 +29,26 @@ export const signInUser = createAsyncThunk(
   }
 );
 
+export const checkAuth = createAsyncThunk(
+  "/auth/check-auth",
+
+  async () => {
+    const response = await axios.get(
+      "http://localhost:5000/api/auth/check-auth",
+
+      {
+        withCredentials: true,
+        headers: {
+          "Cache-Control":
+            "no-store, no-cache, must-revalidate, proxy-revalidate",
+        },
+      }
+    );
+
+    return response.data;
+  }
+);
+
 export const authSlice = createSlice({
   name: "auth",
   initialState: {
@@ -63,6 +83,19 @@ export const authSlice = createSlice({
         state.isAuthenticated = action.payload.success;
       })
       .addCase(signInUser.rejected, (state, action) => {
+        state.isLoading = false;
+        state.user = null;
+        state.isAuthenticated = false;
+      })
+      .addCase(checkAuth.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(checkAuth.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.user = action.payload.success ? action.payload.user : null;
+        state.isAuthenticated = action.payload.success;
+      })
+      .addCase(checkAuth.rejected, (state, action) => {
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
