52213f45d6223fe303290867ef0a8b2e465e9cb5
Asad Babayev <esedbabayev12@gmail.com>
2024-10-29 23:25:44 +0400

    add & send open state

--- src/components/admin/layout.tsx
+++ src/components/admin/layout.tsx
@@ -1,17 +1,20 @@
 import { Outlet } from "react-router-dom";
+import { useState } from "react";
 
 // Components
 import SideBar from "./sideBar";
 import Header from "./header";
 
 const AdminLayout = () => {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
   return (
     <div className="flex min-h-screen w-full">
       {/* sidebar */}
-      <SideBar />
+      <SideBar open={openSidebar} setOpen={setOpenSidebar} />
       <div className="flex flex-1 flex-col">
         {/* header */}
-        <Header />
+        <Header setOpen={setOpenSidebar} />
         <main className="flex flex-1 bg-amber-500 p-4 md:p-6 ">
           <Outlet />
         </main>
