066cfba29767509475c71ee2219a4065f53afb4f
Asad Babayev <esedbabayev12@gmail.com>
2024-10-31 00:59:22 +0400

    maintain image drag & drop

--- src/components/admin/ImageUpload.tsx
+++ src/components/admin/ImageUpload.tsx
@@ -1,12 +1,81 @@
+import { useRef } from "react";
 import { Input } from "../ui/input";
 import { Label } from "../ui/label";
+import { FileIcon, UploadCloudIcon, XIcon } from "lucide-react";
+import { Button } from "../ui/button";
 
-const ImageUpload = () => {
+const ImageUpload = ({
+  image,
+  setImage,
+  uploadedImageUrl,
+  setUploadedImageUrl,
+}) => {
+  const inputRef = useRef(null);
+
+  const imageFileChangeHandler = (event: any) => {
+    console.log(event.target.files);
+    const selectedImage = event.target.files?.[0];
+
+    if (selectedImage) setImage(selectedImage);
+  };
+
+  const dragOverHandler = (event: any) => {
+    event.preventDefault();
+  };
+
+  const dropHandler = (event: any) => {
+    event.preventDefault();
+
+    const droppedFile = event.dataTransfer.files?.[0];
+    if (droppedFile) setImage(droppedFile);
+  };
+
+  const removeImageHandler = () => {
+    setImage(null);
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  };
   return (
-    <div className="w-full max-w-md mx-auto">
+    <div className="w-full max-w-md mx-auto mt-4">
       <Label className="text-lg font-semibold mb-2 block">Upload Image</Label>
-      <div>
-        <Input id="image-upload" type="file" className="hidden" />
+      <div
+        onDragOver={dragOverHandler}
+        onDrop={dropHandler}
+        className="border-2 border-dashed rounded-lg p-4"
+      >
+        <Input
+          id="image-upload"
+          type="file"
+          className="hidden"
+          ref={inputRef}
+          onChange={imageFileChangeHandler}
+        />
+        {!image ? (
+          <Label
+            htmlFor="image-upload"
+            className="flex flex-col items-center justify-center h-32 cursor-pointer"
+          >
+            <UploadCloudIcon className="w-10 h-10 text-muted-foreground mb-2" />
+            <span>Drag & Drop or click to upload</span>
+          </Label>
+        ) : (
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <FileIcon className="w-8 h-8 text-primary mr-2" />
+            </div>
+            <p className="text-sm font-medium">{image.name}</p>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="text-black hover:text-gray-500"
+              onClick={removeImageHandler}
+            >
+              <XIcon className="w-4 h-4" />
+              <span className="sr-only">Remove File</span>
+            </Button>
+          </div>
+        )}
       </div>
     </div>
   );
