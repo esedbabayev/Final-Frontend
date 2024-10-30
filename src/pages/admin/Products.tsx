a239503fbe0a8e6aa4ff7429bef379931f1403fc
Asad Babayev <esedbabayev12@gmail.com>
2024-10-30 23:23:43 +0400

    add right side bar for adding product

--- src/pages/admin/Products.tsx
+++ src/pages/admin/Products.tsx
@@ -1,10 +1,49 @@
+import { useState } from "react";
+// Components
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import Form from "@/components/common/Form";
+
+import { productFormElements } from "@/config";
 
 const Products = () => {
-  return (
-    <div>
-        products
-    </div>
-  )
-}
+  const [openCreateProductView, setOpenCreateProductView] = useState(false);
 
-export default Products
+  const [formData, setFormData] = useState(initialFormData)
+  return (
+    <>
+      <div className="mb-5 w-full flex justify-end">
+        <Button onClick={() => setOpenCreateProductView(true)}>
+          Add New Product
+        </Button>
+      </div>
+      <div className="grid gap-4 md:grid-cols-3 lg:grid-cols-4"></div>
+      <Sheet
+        open={openCreateProductView}
+        onOpenChange={() => setOpenCreateProductView(false)}
+      >
+        <SheetContent side="right" className="overflow-auto">
+          <SheetHeader>
+            <SheetTitle>Add New Product</SheetTitle>
+          </SheetHeader>
+          <div className="py-6">
+            <Form
+              formControls={productFormElements}
+              formData={}
+              setFormData={}
+              onSubmit={}
+              buttonText={}
+            />
+          </div>
+        </SheetContent>
+      </Sheet>
+    </>
+  );
+};
+
+export default Products;
