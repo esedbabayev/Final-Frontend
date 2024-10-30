4e8d6a9bb896ee173f0aaa337eee178de415eb69
Asad Babayev <esedbabayev12@gmail.com>
2024-10-30 23:58:07 +0400

    create image upload component

---
+++ src/components/admin/ImageUpload.tsx
@@ -0,0 +1,15 @@
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
+
+const ImageUpload = () => {
+  return (
+    <div className="w-full max-w-md mx-auto">
+      <Label className="text-lg font-semibold mb-2 block">Upload Image</Label>
+      <div>
+        <Input id="image-upload" type="file" className="hidden" />
+      </div>
+    </div>
+  );
+};
+
+export default ImageUpload;
