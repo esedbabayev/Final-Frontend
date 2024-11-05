f075c2d423c8c118f909f280b3eb3ac9df6e58d3
Asad Babayev <esedbabayev12@gmail.com>
2024-11-05 23:46:07 +0400

    insert filter component

--- src/pages/user/ProductListing.tsx
+++ src/pages/user/ProductListing.tsx
@@ -1,10 +1,12 @@
+// Components
+import Filter from "@/components/user/Filter";
 
 const ProductListing = () => {
   return (
-    <div>
-      Product listing page
+    <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
+      <Filter />
     </div>
-  )
-}
+  );
+};
 
-export default ProductListing
+export default ProductListing;
