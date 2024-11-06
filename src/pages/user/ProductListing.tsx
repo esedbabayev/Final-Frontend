f41e3dc25b42f701ca11e58cfe7194f43a9738bb
Asad Babayev <esedbabayev12@gmail.com>
2024-11-06 23:38:44 +0400

    add sort by section

--- src/pages/user/ProductListing.tsx
+++ src/pages/user/ProductListing.tsx
@@ -1,10 +1,52 @@
 // Components
 import Filter from "@/components/user/Filter";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+// Icons
+import { ArrowUpDownIcon } from "lucide-react";
+
+import { sortOptions } from "@/config/index.js";
 
 const ProductListing = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
       <Filter />
+      <div className="w-full rounded-lg shadow-sm">
+        <div className="p-4 border-b flex items-center justify-between">
+          <h2 className="text-lg font-extrabold mr-2">All products</h2>
+          <div className="flex items-center gap-3">
+            <span className="text-gray-600">10 Products</span>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant={"outline"}
+                  size={"sm"}
+                  className="flex items-center gap-1"
+                >
+                  <ArrowUpDownIcon className="h-4 w-4" />
+                  <span>Sort by</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end" className="w-[200px]">
+                <DropdownMenuRadioGroup>
+                  {sortOptions.map((sortOption) => (
+                    <DropdownMenuRadioItem key={sortOption.id}>
+                      {sortOption.label}
+                    </DropdownMenuRadioItem>
+                  ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
