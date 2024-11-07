164aef8ffd627151f149b2a7da87baec9bd7e0cc
Asad Babayev <esedbabayev12@gmail.com>
2024-11-08 01:20:39 +0400

    add slice

--- src/store/store.js
+++ src/store/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 // Slices
 import { authSlice } from "./slices/auth-slice.js";
 import { adminProductSlice } from "./admin/products.slice.js";
+import { userProductSlice } from "./user/products.slice.js";
 
 const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     adminProducts: adminProductSlice.reducer,
+    userProducts: userProductSlice.reducer,
   },
 });
 
