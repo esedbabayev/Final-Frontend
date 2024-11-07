5bf818da894957923d24c413ec88b8f0003e432b
Asad Babayev <esedbabayev12@gmail.com>
2024-11-08 01:20:32 +0400

    create user products slice

---
+++ src/store/user/products.slice.js
@@ -0,0 +1,39 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  products: [],
+};
+
+export const getAllFilteredProducts = createAsyncThunk(
+  "/products/getallproducts",
+  async () => {
+    console.log(getAllFilteredProducts);
+    const response = await axios.get(
+      "http://localhost:5000/api/shop/products/get"
+    );
+    return response?.data;
+  }
+);
+
+export const userProductSlice = createSlice({
+  name: "userProducts",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getAllFilteredProducts.pending, (state, action) => {
+        state.isLoading = true;
+      })
+      .addCase(getAllFilteredProducts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.products = action.payload.data;
+        console.log(action.payload, "action.payload");
+      })
+      .addCase(getAllFilteredProducts.rejected, (state, action) => {
+        state.isLoading = false;
+        state.products = [];
+      });
+  },
+});
