d3fceec35991b111d98e7e92a570d790889360cc
Asad Babayev <esedbabayev12@gmail.com>
2024-11-08 18:18:36 +0400

    render products & add filter logic

--- src/pages/user/ProductListing.tsx
+++ src/pages/user/ProductListing.tsx
@@ -1,5 +1,5 @@
 // Hooks
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 // Actions
@@ -20,12 +20,49 @@ import {
 import { ArrowUpDownIcon } from "lucide-react";
 
 import { sortOptions } from "@/config/index.js";
+import UserProducts from "@/components/user/Products";
 
 const ProductListing = () => {
   const dispatch = useDispatch();
   const { products } = useSelector((state) => state.userProducts);
 
-  console.log(products, "products");
+  const [filters, setFilters] = useState({});
+  const [sort, setSort] = useState(null);
+
+  // console.log(products, "products");
+
+  const sortHandler = (value) => {
+    console.log(value, "value");
+    setSort(value);
+  };
+
+  const filterHandler = (getSectionId, getCurrentOption) => {
+    let copyFilters = { ...filters };
+    const indexOfCurrentSection =
+      Object.keys(copyFilters).indexOf(getSectionId);
+
+    if (indexOfCurrentSection === -1) {
+      copyFilters = {
+        ...copyFilters,
+        [getSectionId]: [getCurrentOption],
+      };
+    } else {
+      const indexOfCurrentOption =
+        copyFilters[getSectionId].indexOf(getCurrentOption);
+
+      if (indexOfCurrentOption === -1)
+        copyFilters[getSectionId].push(getCurrentOption);
+      else copyFilters[getSectionId].splice(indexOfCurrentOption, 1);
+    }
+    setFilters(copyFilters);
+    localStorage.setItem("filters", JSON.stringify(copyFilters));
+  };
+  console.log(filters, "filters");
+
+  useEffect(() => {
+    setSort("price-lowtohigh");
+    setFilters(JSON.parse(localStorage.getItem("filters")) || {});
+  }, []);
 
   // Fetch products
   useEffect(() => {
@@ -33,13 +70,13 @@ const ProductListing = () => {
   }, [dispatch]);
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-[300px_1fr] gap-6 p-4 md:p-6">
-      <Filter />
+    <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
+      <Filter filters={filters} filterHandler={filterHandler} />
       <div className="w-full rounded-lg shadow-sm">
         <div className="p-4 border-b flex items-center justify-between">
           <h2 className="text-lg font-extrabold mr-2">All products</h2>
           <div className="flex items-center gap-3">
-            <span className="text-gray-600">10 Products</span>
+            <span className="text-gray-600">{products?.length} Products</span>
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
                 <Button
@@ -52,9 +89,15 @@ const ProductListing = () => {
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end" className="w-[200px]">
-                <DropdownMenuRadioGroup>
+                <DropdownMenuRadioGroup
+                  value={sort}
+                  onValueChange={sortHandler}
+                >
                   {sortOptions.map((sortOption) => (
-                    <DropdownMenuRadioItem key={sortOption.id}>
+                    <DropdownMenuRadioItem
+                      value={sortOption.id}
+                      key={sortOption.id}
+                    >
                       {sortOption.label}
                     </DropdownMenuRadioItem>
                   ))}
@@ -63,7 +106,13 @@ const ProductListing = () => {
             </DropdownMenu>
           </div>
         </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4"></div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4">
+          {products && products.length > 0
+            ? products.map((product) => {
+                return <UserProducts key={product?._id} product={product} />;
+              })
+            : null}
+        </div>
       </div>
     </div>
   );
