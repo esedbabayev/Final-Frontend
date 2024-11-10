9fcbfa7fe204c691b282af388ace9f2165693044
Asad Babayev <esedbabayev12@gmail.com>
2024-11-10 17:43:58 +0400

    create cart item content component

--- src/components/user/CartContent.tsx
+++ src/components/user/CartContent.tsx
@@ -1,10 +1,67 @@
+// Hooks
+import { useDispatch, useSelector } from "react-redux";
 
-const CartContent = () => {
+// Actions
+import { removeCartItem } from "@/store/user/cart.slice.js";
+
+// Components
+import { Button } from "../ui/button";
+
+// Icons
+import { Minus, Plus, Trash } from "lucide-react";
+
+const CartItemsContent = ({ cartItem }) => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.auth);
+
+  const removeCartItemHandler = (cartItem) => {
+    dispatch(
+      removeCartItem({ userId: user?.id, productId: cartItem?.productId })
+    );
+  };
   return (
-    <div>
-      CartContent
+    <div className="flex items-center space-x-4">
+      <img
+        src={cartItem?.image}
+        alt={cartItem?.title}
+        className="w-20 h-20 rounded object-cover"
+      />
+      <div className="flex-1">
+        <h3 className="font-extrabold">{cartItem?.title}</h3>
+        <div className="flex items-center gap-2 mt-1">
+          <Button
+            variant={"outline"}
+            size={"icon"}
+            className="w-8 h-8 rounded-full"
+          >
+            <Minus className="w-4 h-4" />
+          </Button>
+          <span className="font-semibold">{cartItem?.quantity}</span>
+          <Button
+            variant={"outline"}
+            size={"icon"}
+            className="w-8 h-8 rounded-full"
+          >
+            <Plus className="w-4 h-4" />
+          </Button>
+        </div>
+      </div>
+      <div className="flex flex-col items-end">
+        <p className="font-semibold">
+          $
+          {(
+            (cartItem?.salePrice > 0 ? cartItem?.salePrice : cartItem?.price) *
+            cartItem?.quantity
+          ).toFixed(2)}
+        </p>
+        <Trash
+          onClick={() => removeCartItemHandler(cartItem)}
+          className="cursor-pointer mt-1"
+          size={20}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default CartContent
+export default CartItemsContent;
