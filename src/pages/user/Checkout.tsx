27181070e2ac1bc4bb109d5f063b69ccf9662f3e
Asad Babayev <esedbabayev12@gmail.com>
2024-11-12 23:32:10 +0400

    initiate component

--- src/pages/user/Checkout.tsx
+++ src/pages/user/Checkout.tsx
@@ -1,10 +1,55 @@
+import image from "@/assets/account.jpg";
+import { Button } from "@/components/ui/button";
+import Address from "@/components/user/Address";
+import CartItemsContent from "@/components/user/CartContent";
+import { useSelector } from "react-redux";
 
 const Checkout = () => {
-  return (
-    <div>
-      Checkout
-    </div>
-  )
-}
+  const { cartItems } = useSelector((state) => state.cart);
 
-export default Checkout
+  const totalAmount =
+    cartItems && cartItems.items && cartItems.items.length > 0
+      ? cartItems.items.reduce(
+          (sum, currentItem) =>
+            sum +
+            (currentItem?.salePrice > 0
+              ? currentItem?.salePrice
+              : currentItem?.price) *
+              currentItem?.quantity,
+          0
+        )
+      : 0;
+
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-[300px] w-full overflow-hidden">
+        <img
+          src={image}
+          alt="pic"
+          className="w-full h-full object-cover object-center"
+        />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 mt-5 p-5">
+        <Address />
+        <div className="flex flex-col gap-4">
+          {cartItems && cartItems.items && cartItems.items.length > 0
+            ? cartItems.items.map((cartItem) => (
+                <CartItemsContent key={cartItem?.id} cartItem={cartItem} />
+              ))
+            : null}
+        <div className="mt-8 space-y-4">
+          <div className="flex justify-between">
+            <span className="font-bold">Total</span>
+            <span className="font-bold">${totalAmount}</span>
+          </div>
+        </div>
+        <div className="mt-4 w-full">
+          <Button className="mt-4 w-full">Checkout with PayPal</Button>
+        </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
