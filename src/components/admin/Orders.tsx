beebcaa4bb6a1142191281098ec48baa4481add9
Asad Babayev <esedbabayev12@gmail.com>
2024-11-12 23:30:59 +0400

    create component

---
+++ src/components/admin/Orders.tsx
@@ -0,0 +1,14 @@
+// Components
+import { Card, CardHeader, CardTitle } from "../ui/card";
+
+const AdminOrders = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>All Orders</CardTitle>
+      </CardHeader>
+    </Card>
+  );
+};
+
+export default AdminOrders;
--- src/components/user/Orders.tsx
+++ src/components/user/Orders.tsx
@@ -1,10 +1,27 @@
+// Components
+import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Table, TableHead, TableHeader, TableRow } from "../ui/table";
 
 const Orders = () => {
   return (
-    <div>
-      ord
-    </div>
-  )
-}
+    <Card>
+      <CardHeader>
+        <CardTitle>Order History</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Order ID</TableHead>
+              <TableHead>Order Date</TableHead>
+              <TableHead>Order Status</TableHead>
+              <TableHead>Order Price</TableHead>
+            </TableRow>
+          </TableHeader>
+        </Table>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default Orders
+export default Orders;
