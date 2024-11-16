66130818ee7147c6902a946551918869349ee3d9
Asad Babayev <esedbabayev12@gmail.com>
2024-11-17 00:22:28 +0400

    use common image upload component

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,10 +1,28 @@
+import ImageUpload from "@/components/admin/ImageUpload";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 const Dashboard = () => {
+  const [image, setImage] = useState(null);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
+  const [imageLoadingState, setImageLoadingState] = useState(false);
+
+  console.log(uploadedImageUrl, "uploadedImageUrl");
+  
   return (
     <div>
-        dashboard
+      <ImageUpload
+        image={image}
+        setImage={setImage}
+        uploadedImageUrl={uploadedImageUrl}
+        setUploadedImageUrl={setUploadedImageUrl}
+        imageLoadingState={imageLoadingState}
+        setImageLoadingState={setImageLoadingState}
+        // isEditMode={currentEditedId !== null}
+      />
+      <Button>Upload</Button>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
