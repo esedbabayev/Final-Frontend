4b74139de9f3520550b73b1a1272e7747fe08c40
Asad Babayev <esedbabayev12@gmail.com>
2024-11-18 14:45:39 +0400

    setUploadedImageUrl

--- src/pages/admin/Dashboard.tsx
+++ src/pages/admin/Dashboard.tsx
@@ -1,14 +1,38 @@
 import ImageUpload from "@/components/admin/ImageUpload";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addFeatureImage,
+  getFeatureImages,
+} from "@/store/admin/feature.slice.js";
 
 const Dashboard = () => {
   const [image, setImage] = useState(null);
   const [uploadedImageUrl, setUploadedImageUrl] = useState("");
   const [imageLoadingState, setImageLoadingState] = useState(false);
 
+  const dispatch = useDispatch();
+  const { featureImageList } = useSelector((state) => state.feature);
+
+  const featureImageUploadHandler = () => {
+    dispatch(addFeatureImage(uploadedImageUrl)).then((data) => {
+      if (data?.payload?.success) {
+        dispatch(getFeatureImages());
+        setImage(null);
+        setUploadedImageUrl("");
+      }
+    });
+  };
+
+  useEffect(() => {
+    dispatch(getFeatureImages());
+  }, [dispatch]);
+
+  console.log(featureImageList, "featureImageList");
+
   console.log(uploadedImageUrl, "uploadedImageUrl");
-  
+
   return (
     <div>
       <ImageUpload
@@ -18,9 +42,24 @@ const Dashboard = () => {
         setUploadedImageUrl={setUploadedImageUrl}
         imageLoadingState={imageLoadingState}
         setImageLoadingState={setImageLoadingState}
+        isCustomStyling={true}
         // isEditMode={currentEditedId !== null}
       />
-      <Button>Upload</Button>
+      <Button onClick={featureImageUploadHandler} className="mt-5 w-full">
+        Upload
+      </Button>
+      <div className="flex flex-col gap-4 mt-5">
+        {featureImageList && featureImageList.length > 0
+          ? featureImageList.map((featureImgItem) => (
+              <div className="relative">
+                <img
+                  src={featureImgItem.image}
+                  className="w-full h-[300px] object-cover rounded-t-lg"
+                />
+              </div>
+            ))
+          : null}
+      </div>
     </div>
   );
 };
