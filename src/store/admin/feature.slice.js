2b2120a971e855ea02712ac982ae2f6250248c43
Asad Babayev <esedbabayev12@gmail.com>
2024-11-18 14:44:56 +0400

    create slice

---
+++ src/store/admin/feature.slice.js
@@ -0,0 +1,50 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+  isLoading: false,
+  featureImageList: [],
+};
+
+export const getFeatureImages = createAsyncThunk(
+  "/order/getFeatureImages",
+  async () => {
+    const response = await axios.get(
+      `http://localhost:5000/api/admin/feature/get`
+    );
+
+    return response.data;
+  }
+);
+
+export const addFeatureImage = createAsyncThunk(
+  "/order/addFeatureImage",
+  async (image) => {
+    const response = await axios.post(
+      `http://localhost:5000/api/admin/feature/add`,
+      { image }
+    );
+
+    return response.data;
+  }
+);
+
+export const featureSlice = createSlice({
+  name: "featureSlice",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(getFeatureImages.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getFeatureImages.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.featureImageList = action.payload.data;
+      })
+      .addCase(getFeatureImages.rejected, (state) => {
+        state.isLoading = false;
+        state.featureImageList = [];
+      });
+  },
+});
